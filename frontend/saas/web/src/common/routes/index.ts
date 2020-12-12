1b9d0a3cff173bdfe84d492215a598a156bab7b9
Alexey Nikitin <kvandake@gmail.com>
2020-12-12 14:21:31 +0300

    WIP: Some fix

--- frontend/saas/web/src/common/routes/index.ts
+++ frontend/saas/web/src/common/routes/index.ts
@@ -1,6 +1,5 @@
 export const APP_PATH = "/app";
 export const APP_HOME_ROUTE = `${APP_PATH}`
-export const APP_HOMEWORK_ROUTE = `${APP_PATH}/homework`
 export const APP_SETTINGS_ROUTE = `${APP_PATH}/settings`
 
 export const LOGIN_ROUTE = `/login`
